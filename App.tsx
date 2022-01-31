5e618a2b8e5bc4dc3bd2ec810d74ebc80385fdc6
Marcin Bednarz <94953520+Yerbaneyro@users.noreply.github.com>
2022-01-31 21:46:22 +0100

    Restructur of Apollo Provider
    Apolo provider and token conector moved to the root in tree so server data should be avalible for all app right now.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import RoomsScreen from './screens/rooms';
+import { setContext } from 'apollo-link-context'
+import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider, useQuery } from '@apollo/client';
 
+
+
+const httpLink = createHttpLink({
+    uri: 'https://chat.thewidlarzgroup.com/api/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJjaGF0bHkiLCJleHAiOjE2NDU1MjMzMzIsImlhdCI6MTY0MzEwNDEzMiwiaXNzIjoiY2hhdGx5IiwianRpIjoiZmY0MjQxMjItNmQ4OS00ZTIxLTk5NzktMTJjMWFlNGYyMmMzIiwibmJmIjoxNjQzMTA0MTMxLCJzdWIiOiJiMzIwZWJiYi0wMmI5LTRmNzgtYmE5Yy1kMjY0NzQyMjA1YWEiLCJ0eXAiOiJhY2Nlc3MifQ.DdXRS-Hr4q3gKMLSIlN2DH0TeVc0kjw885A67dPn4_IPho70HEK91EjyGAiF3OWyFmCk9N9-g01FcQ7I3aPHbw';
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+});
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <RoomsScreen />
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <RoomsScreen />
+        <StatusBar style="auto" />
+      </View>
+    </ApolloProvider>
   );
 }
 
--- screens/rooms.tsx
+++ screens/rooms.tsx
@@ -4,31 +4,7 @@ import { useFonts } from 'expo-font';
 import { SearchSvg, RoomsSvg } from '../components/svg-icons';
 import Rooms from '../components/single-room';
 import { GetRooms } from '../graphql/get-room';
-import { setContext } from 'apollo-link-context'
-import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider, useQuery } from '@apollo/client';
-
-
-
-const httpLink = createHttpLink({
-    uri: 'https://chat.thewidlarzgroup.com/api/graphql'
-});
-
-const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists
-    const token = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJjaGF0bHkiLCJleHAiOjE2NDU1MjMzMzIsImlhdCI6MTY0MzEwNDEzMiwiaXNzIjoiY2hhdGx5IiwianRpIjoiZmY0MjQxMjItNmQ4OS00ZTIxLTk5NzktMTJjMWFlNGYyMmMzIiwibmJmIjoxNjQzMTA0MTMxLCJzdWIiOiJiMzIwZWJiYi0wMmI5LTRmNzgtYmE5Yy1kMjY0NzQyMjA1YWEiLCJ0eXAiOiJhY2Nlc3MifQ.DdXRS-Hr4q3gKMLSIlN2DH0TeVc0kjw885A67dPn4_IPho70HEK91EjyGAiF3OWyFmCk9N9-g01FcQ7I3aPHbw';
-    // return the headers to the context so httpLink can read them
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : "",
-        }
-    }
-});
-
-const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
-});
+import { useQuery } from '@apollo/client';
 
 const RoomsData = () => {
     const { data, loading} = useQuery(GetRooms);
@@ -52,13 +28,11 @@ export default function RoomsScreen() {
     }
 
     return (
-        <ApolloProvider client={client}>
-            <View>
-                <Header />
-                <Rooms />
-                <RoomsData />
-            </View>
-        </ApolloProvider>
+        <View>
+            <Header />
+            <Rooms />
+            <RoomsData />
+        </View>
     );
 }
 
